c26ed1ff1ec95aacc3b5fd57bbb258c203f3d088
Sheriff Alhassan <dayo.hassan@gmail.com>
2019-07-25 17:45:38 -0400

    adding to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function(){
     // document.write("hello world")
-    var animalsArray = ["lion", "elephant", "horse"];
+    var animalsArray = ["lion", "elephant", "horse", "zebra"];
     for(var i =0; i<animalsArray.length; i++){
         var button = $("<button>")
         button.text(animalsArray[i])
         $("#buttons").append(button)
     }
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qP6wOXAK4ndbnLTMckJsw0A3fL7AqjVd&q=horse&limit=25&offset=0&rating=G&lang=en"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qP6wOXAK4ndbnLTMckJsw0A3fL7AqjVd&q=horse&limit=10"
 
     $.ajax({
         url: queryURL,
@@ -17,6 +17,49 @@ $(document).ready(function(){
     })
 })
 
+$("#animals-view").empty();
+
+for (var i = 0; i<response.data.length; i++){
+
+  var animalsDiv = $("#<div class='animals'>");
+
+  var rating = response.data[i].rating;
+  var pOne = $('<p>').text("Rating: " + rating);
+
+  animalsDiv.append(pOne);
+}
+
+
+
+
+// function displayAnimalGif() {
+//   console.log($(this));
+//   var animal = $(this).attr("data-name");
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=qP6wOXAK4ndbnLTMckJsw0A3fL7AqjVd&q&limit=10";
+
+//   // Make an AJAX call for the specific animal button being clicked on
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function (response) {
+
+//     console.log(response);
+
+//     $("#animal-view").empty();
+
+//     for (var i = 0; i < response.data.length; i++) {
+
+//       // Create a div here to hold the animal
+//       var animalDiv = $("<div class='animal'>");
+
+//       // Store the rating data
+//       var rating = response.data[i].rating;
+
+//       // Make an element to have the rating displayed
+//       var pOne = $("<p>").text("Rating: " + rating);
+
+//       // Display the rating
+//       animalDiv.append(pOne);
 
 
 
